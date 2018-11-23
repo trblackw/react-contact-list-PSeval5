b01ca4cf392b25e38e70b8e65981c964ec38feec
Tucker <trblackw@ncsu.edu>
2018-11-23 17:37:44 -0500

    built and added routes for place holder components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Contacts from "./components/Contacts";
+import NewContact from "./components/NewContact";
 
 const App = () => (
   <Fragment>
     <Router>
       <Switch>
         <Route exact path="/" component={Contacts} />
+        <Route path="/new" component={NewContact} />
       </Switch>
     </Router>
   </Fragment>
---
+++ src/components/AddContact.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class AddContact extends Component {
+   state = {
+      
+   }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
